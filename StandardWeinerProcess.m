bd09e106f5e99f13accc72a22586887ec0c1da89
Vaibhav Ajmera <51504937+vajmera@users.noreply.github.com>
2020-08-08 10:12:04 +0530

    Add files via upload

---
+++ StandardWeinerProcess.m
@@ -0,0 +1,26 @@
+% % % Q.1
+%Simulates a sample path {W(t)} t ∈ [0,1], for a standard Wiener process,
+% by sampling the process 1000 times per unit of time, 
+% so that increments are N(0, 0.001) distributed and show the result with a plot.
+
+
+% % clear all;
+% % i=0; 
+% % N=1000;                                    %assuming intervals of same length 
+% % T=1/N;
+% % y(1)=sqrt(T)*normrnd(0,0.01);             %normal random variable with
+%                                            % mean 0 and variance .01*sqrt(T)
+% % x(1)=y(1);                                   
+% %    for i=1:N
+% %      y(i+1)=sqrt(T)*normrnd(0,0.01);      %increment 
+% %      x(i+1)=x(i)+y(i+1);                   
+% %    end
+% % t=1:N+1;
+% % plot(t,x);
+% % xlabel('Time Line');
+% % ylabel('Sample Path');
+% % grid on;
+% 
+% 
+% 
+
